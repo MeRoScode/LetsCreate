505ba3363093273dac562d197637eb6acd52854f
MehranMr17 <Mrs7Mehrann@mail.com>
2023-09-14 22:29:05 +0430

    Constants file created to store static fields

---
+++ app/src/main/java/com/meros/letscreate/Constants.java
@@ -0,0 +1,10 @@
+package com.meros.letscreate;
+
+import android.speech.tts.TextToSpeech;
+
+public class Constants {
+
+    public static TextToSpeech textToSpeech;
+    public static int REQ_CODE_SPEECH_INPUT = 1;
+
+}
