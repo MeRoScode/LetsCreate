9425040904a7e8f1da74a417b902ed9e82676894
MehranMr17 <Mrs7Mehrann@mail.com>
2023-09-16 23:51:30 +0430

    Database constants are created

--- app/src/main/java/com/meros/letscreate/Constants.java
+++ app/src/main/java/com/meros/letscreate/Constants.java
@@ -1,10 +1,19 @@
 package com.meros.letscreate;
 
+import android.content.Context;
 import android.speech.tts.TextToSpeech;
+
+import com.meros.letscreate.Fragments.Calender.MangeTackTable;
 
 public class Constants {
 
     public static TextToSpeech textToSpeech;
     public static int REQ_CODE_SPEECH_INPUT = 1;
+    public static MangeTackTable mangeTackTable;
+    public static final String TACK_TB = "tack_tb";
 
+    public static MangeTackTable getMangeTackTable(Context c) {
+        if (mangeTackTable == null) mangeTackTable = new MangeTackTable(c);
+        return mangeTackTable;
+    }
 }
